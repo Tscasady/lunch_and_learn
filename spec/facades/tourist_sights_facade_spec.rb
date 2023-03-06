6592daa36de5a855e3561eff8b146e069be9b705
Thomas Casady <tscasady@gmail.com>
2023-03-06 11:55:25 -0600

    Add vcr to tourist_sights facade test.

--- spec/facades/tourist_sights_facade_spec.rb
+++ spec/facades/tourist_sights_facade_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe TouristSightsFacade do
-  it 'can return an array of TouristSight objects' do
+  it 'can return an array of TouristSight objects', :vcr do
     tourist_sights = TouristSightsFacade.sights('thailand')
     expect(tourist_sights).to be_a Array
     tourist_sights.each do |tourist_sight|
