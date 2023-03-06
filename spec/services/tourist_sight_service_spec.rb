360a022481d3a7674e637fbf6353890946d41fe4
Thomas Casady <tscasady@gmail.com>
2023-03-06 11:22:11 -0600

    Update toursit service test to properly reflect return data.

--- spec/services/tourist_sight_service_spec.rb
+++ spec/services/tourist_sight_service_spec.rb
@@ -4,15 +4,18 @@ RSpec.describe TouristSightService do
   describe 'class methods' do
     describe 'sights' do
       it 'returns a hash of tourist sight data' do
-        tourist_sights = TouristSightService.sights('Bosnia')
+        #Coords for Sarajevo
+        lat = 43.8 
+        lng = 18.4 
+        tourist_sights = TouristSightService.sights(lat, lng)
         
         expect(tourist_sights).to be_a Hash
-        expect(tourist_sights[:hits]).to be_a Array
-        expect(tourist_sights[:hits][0]).to be_a Hash
-        expect(tourist_sights[:hits][0][:recipe]).to be_a Hash
-        expect(tourist_sights[:hits][0][:recipe][:url]).to be_a String
-        expect(tourist_sights[:hits][0][:recipe][:label]).to be_a String
-        expect(tourist_sights[:hits][0][:recipe][:image]).to be_a String
+        expect(tourist_sights[:features]).to be_a Array
+        expect(tourist_sights[:features][0]).to be_a Hash
+        expect(tourist_sights[:features][0][:properties]).to be_a Hash
+        expect(tourist_sights[:features][0][:properties][:name]).to be_a String
+        expect(tourist_sights[:features][0][:properties][:formatted]).to be_a String
+        expect(tourist_sights[:features][0][:properties][:place_id]).to be_a String
       end
     end
   end
