6be80fe805baff6a97d52f8a15ebbe3e8d075792
Thomas Casady <tscasady@gmail.com>
2023-03-07 14:30:23 -0600

    Add test for get favorites request.

--- spec/request/api/v1/favorites/favorite_request_spec.rb
+++ spec/request/api/v1/favorites/favorite_request_spec.rb
@@ -39,4 +39,44 @@ RSpec.describe 'The favorite resources request', type: :request do
       expect(user.favorites).to eq []
     end
   end
+
+  describe 'get favorite endpoint' do
+    it 'can return a list of favorites for a given user api_key' do
+      user = User.create!(name: 'test', email: 'test@email.com', api_key: 'jgn983hy48thw9begh98h4539h4')
+      Favorite.create!(country: 'Brazil', recipe_title: 'food thing', recipe_link: 'recipe.com', user: user)
+      Favorite.create!(country: 'Canada', recipe_title: 'good meal', recipe_link: 'recipe.com', user: user)
+      Favorite.create!(country: 'Mexio', recipe_title: 'tasty dinner', recipe_link: 'recipe.com', user: user)
+      get "/api/v1/favorites?api_key=#{user.api_key}"
+
+      expect(response.status).to be 200
+
+      favorites = JSON.parse(response.body, symbolize_names: true)[:data]
+
+      expect(favorites).to be_a Array
+      expect(favorites.length).to eq 3
+
+      favorites.each do |favorite|
+        expect(favorite[:id]).to be_a Numeric
+        expect(favorite[:id][:type]).to eq 'favorite'
+        expect(favorite[:id][:attributes]).to be_a Hash
+        expect(favorite[:id][:attributes][:recipe_title]).to be_a String
+        expect(favorite[:id][:attributes][:recipe_link]).to be_a String
+        expect(favorite[:id][:attributes][:country]).to be_a String
+        expect(favorite[:id][:attributes][:created_at]).to be_a String
+      end
+
+      expect(favorite.first[:country]).to eq 'Brazil'
+      expect(favorite.last[:country]).to eq 'Mexico'
+    end
+
+    it 'can return an empty array if the user has not favorited any recipes' do
+      user = User.create!(name: 'test', email: 'test@email.com', api_key: 'jgn983hy48thw9begh98h4539h4')
+      get "/api/v1/favorites?api_key=#{user.api_key}"
+
+      expect(response.status).to be 200
+
+      favorites = JSON.parse(response.body, symbolize_names: true)[:data]
+      expect(favorites).to eq []
+    end
+  end
 end
