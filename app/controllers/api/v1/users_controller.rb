c641b2d0517e05be2a9cdd9fa8fb0c66e4a4aa08
Thomas Casady <tscasady@gmail.com>
2023-03-07 10:18:24 -0600

    Add api key generator.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,17 @@ module Api
   module V1
     class UsersController < ApplicationController
       def create
+        User.create!(user_params)
+      end
 
+      private 
+
+      def user_params
+        params.require(:user).permit(:name, :email).merge(api_key: key)
+      end
+
+      def key
+        ApiKeyGenerator.new.key
       end
     end
   end
--- spec/request/api/v1/users/user_request_spec.rb
+++ spec/request/api/v1/users/user_request_spec.rb
@@ -3,7 +3,11 @@ require 'rails_helper'
 RSpec.describe 'The user resources request', type: :request do
   describe 'provides a post endpoint for adding a user' do
     it 'can add a user to the database with an id, name, email, and apikey' do
-      post '/api/v1/users' 
+      post '/api/v1/users', params: { user: { name: 'test', email: 'test@email.com'} }
+
+
+
+      
     end
   end
 end
