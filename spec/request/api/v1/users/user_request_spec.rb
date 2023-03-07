f2401ba6b5a5ae0297da32111336581828f8350d
Thomas Casady <tscasady@gmail.com>
2023-03-06 22:25:33 -0600

    Add user post request spec.

---
+++ spec/request/api/v1/users/user_request_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe 'The user resources request', type: :request do
+  describe 'provides a post endpoint for adding a user' do
+    it 'can add a user to the database with an id, name, email, and apikey' do
+      post '/api/v1/users' 
+    end
+  end
+end
+
