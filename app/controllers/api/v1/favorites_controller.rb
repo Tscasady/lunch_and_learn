cf791b1831b96dbbcffcccbd1420dd3f7a962ca7
Thomas Casady <tscasady@gmail.com>
2023-03-07 12:29:22 -0600

    Add error handling to favorite creation

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -2,13 +2,16 @@ module Api
   module V1
     class FavoritesController < ApplicationController
       def create
-        render json: FavoriteSerializer.new(Favorite.create!(favorite_params)), status: 201
+        user = User.find_by(api_key: params[:favorite][:api_key])
+        Favorite.create!(favorite_params(user))
+        # user.favorites.create!(favorite_params)
+        render json: { success: 'Favorite added successfully' }, status: 201
       end
 
       private 
 
-      def favorite_params
-        params.require(:favorite)
+      def favorite_params(user)
+        params.require(:favorite).permit(:country, :recipe_link, :recipe_title).merge(user: user)
       end
     end
   end
--- spec/request/api/v1/favorites/favorite_request_spec.rb
+++ spec/request/api/v1/favorites/favorite_request_spec.rb
@@ -2,23 +2,41 @@ require 'rails_helper'
 
 RSpec.describe 'The favorite resources request', type: :request do
   describe 'provides a post endpoint for adding a favorite recipe to a user' do
-    it '' do
-      post '/api/v1/favorites', params: {
+    it 'can create a new favorite recipe for a user' do
+      user = User.create!(name: 'test', email: 'test@email.com', api_key: 'jgn983hy48thw9begh98h4539h4')
+      post '/api/v1/favorites', params: { favorite: {
         "api_key": 'jgn983hy48thw9begh98h4539h4',
         "country": 'thailand',
         "recipe_link": 'https://www.tastingtable.com/',
         "recipe_title": 'Crab Fried Rice (Khaao Pad Bpu)'
-      }
+      } }
 
       expect(response.status).to be 201
       message = JSON.parse(response.body, symbolize_names: true)
       expect(message[:success]).to eq 'Favorite added successfully'
 
       favorite = Favorite.last
-      expect(favorite.api_key).to eq 'jgn983hy48thw9begh98h4539h4'
+      expect(user.favorites).to eq [favorite]
       expect(favorite.country).to eq 'thailand'
       expect(favorite.recipe_link).to eq 'https://www.tastingtable.com/'
       expect(favorite.recipe_title).to eq 'Crab Fried Rice (Khaao Pad Bpu)'
+    end
+
+    it 'can return an error if user does not exist or cannot be found with provided api_key' do
+      user = User.create!(name: 'test', email: 'test@email.com', api_key: 'jgn983hy48thw9begh98h4539h4')
+      post '/api/v1/favorites', params: { favorite: {
+        "api_key": 'badkey',
+        "country": 'thailand',
+        "recipe_link": 'https://www.tastingtable.com/',
+        "recipe_title": 'Crab Fried Rice (Khaao Pad Bpu)'
+      } }
+
+      expect(response.status).to be 422
+      message = JSON.parse(response.body, symbolize_names: true)
+      expect(message[:user]).to eq ['must exist']
+
+      expect(Favorite.all.count).to eq 0
+      expect(user.favorites).to eq []
     end
   end
 end
