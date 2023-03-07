8a245de87439707f650896cfb4c816ce995ef93b
Thomas Casady <tscasady@gmail.com>
2023-03-06 22:33:42 -0600

    Add user model and validations.

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'relations' do
+
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_uniqueness_of :email }
+    it { should validate_uniqueness_of :api_key }
+  end
+end
