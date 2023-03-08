84ed2e0722775d03eddb9125104494ba539a93ba
Thomas Casady <tscasady@gmail.com>
2023-03-07 18:06:01 -0600

    Remove unnecessary if condition for empty string country searches.

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -2,12 +2,7 @@ module Api
   module V1
     class RecipesController < ApplicationController
       def index
-        if params[:country] == ''
-          render json: { data: [] }
-        else
-          recipes = RecipeFacade.recipes(country) 
-          render json: RecipeSerializer.new(recipes) 
-        end
+        render json: RecipeSerializer.new(RecipeFacade.recipes(country) ) 
       end
 
       private 
